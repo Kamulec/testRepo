3de87313b8b531b5986afc3caac9529461605cae
Kamulec <kamulec@gmail.com>
2013-08-24 01:15:38 +0200

    message: vs -> v3

--- egitTest/src/Main.java
+++ egitTest/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This version differs from the initial");
-		System.out.println("Even more v2");
+		System.out.println("Even more v3");
 	}
 }
